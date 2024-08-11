348907b68de2fb83d4287dce009e02cb8c7cfd2c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-08-11 18:22:07 -0300

    Create AcademicProgram.java

---
+++ src/main/java/com/flower/portfolio/model/AcademicProgram.java
@@ -0,0 +1,32 @@
+package com.flower.portfolio.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="academic_programs")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AcademicProgram {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 30)
+    private String name;
+    @Column(nullable = false, length = 50)
+    private String institution;
+    private String degreeType;
+    @Column(nullable = false)
+    private LocalDate startDate;
+    private LocalDate endDate;
+    @Column(nullable = false)
+    private Status status;
+
+}
