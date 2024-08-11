88b2ae57f4466609321ea4bdea247713c1429e05
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-08-11 19:14:42 -0300

    Create WebProject.java

---
+++ src/main/java/com/flower/portfolio/model/WebProject.java
@@ -0,0 +1,32 @@
+package com.flower.portfolio.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name="projects")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WebProject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 20)
+    private String title;
+    private String description;
+    private String url;
+    private Status status;
+    private String imageURL;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.MERGE)
+    @JsonIgnoreProperties(value="project")
+    private List<Technology> technologies;
+}
