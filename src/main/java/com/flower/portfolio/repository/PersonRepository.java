eaea8912e48f5cfd4c466bc5b9fb30b113acec06
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-03 18:39:19 -0300

    Create PersonRepository.java

---
+++ src/main/java/com/flower/portfolio/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.flower.portfolio.repository;
+
+import com.flower.portfolio.model.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person,Long> {
+
+    Person findByLastname(String lastname);
+}
