fde2592a019c1af3046293f7f96d7b7fdaf74321
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-03 18:39:37 -0300

    Create PersonDTO.java

---
+++ src/main/java/com/flower/portfolio/dto/PersonDTO.java
@@ -0,0 +1,13 @@
+package com.flower.portfolio.dto;
+
+import java.time.LocalDate;
+
+public class PersonDTO {
+    private String name;
+    private String lastName;
+    private String tel;
+    private LocalDate dateOfBirth;
+    private String occupation;
+    private String presentation;
+
+}
