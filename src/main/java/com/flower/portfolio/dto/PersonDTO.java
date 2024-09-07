e9d652012709a29dfa12f0d5e0507b3a84a08c74
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-07 17:46:26 -0300

    Update PersonDTO.java

--- src/main/java/com/flower/portfolio/dto/PersonDTO.java
+++ src/main/java/com/flower/portfolio/dto/PersonDTO.java
@@ -1,5 +1,6 @@
 package com.flower.portfolio.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PersonDTO {
+    private Long id;
     @NotNull
     @NotEmpty
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must contain only letters.")
@@ -22,6 +24,8 @@ public class PersonDTO {
     private String lastName;
     @Pattern(regexp = "^\\+?[0-9]+$", message = "Phone number must contain only digits and optionally start with a '+'.")
     private String tel;
+    @Email
+    private String email;
     private LocalDate dateOfBirth;
     @Pattern(regexp = "^[a-zA-Z.()!]+$", message = "Occupation must contain only letters, periods, exclamation marks, and parentheses.")
     private String occupation;
