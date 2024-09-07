0448359cb1edfdd686ea35cf5707673fc82440d6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-07 17:43:47 -0300

    Update IPersonMapper.java

--- src/main/java/com/flower/portfolio/dto/mapper/IPersonMapper.java
+++ src/main/java/com/flower/portfolio/dto/mapper/IPersonMapper.java
@@ -1,10 +1,12 @@
 package com.flower.portfolio.dto.mapper;
 
+import com.flower.portfolio.dto.ContactInfoDTO;
 import com.flower.portfolio.dto.PersonDTO;
 import com.flower.portfolio.model.Person;
-import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+
 public interface IPersonMapper {
+    Person mapToEntity(PersonDTO dto);
     PersonDTO mapToDto(Person person);
+    ContactInfoDTO mapToSensitiveData(Person person);
 }
