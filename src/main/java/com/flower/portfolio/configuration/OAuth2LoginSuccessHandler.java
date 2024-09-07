e897f1693c844ee49a98d676d8b0574abb1183b2
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-07 20:12:42 -0300

    Create OAuth2LoginSuccessHandler.java

---
+++ src/main/java/com/flower/portfolio/configuration/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,25 @@
+package com.flower.portfolio.configuration;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    @Value("${frontend.url}")
+    public String frontUrl;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        this.setAlwaysUseDefaultTargetUrl(true);
+        this.setDefaultTargetUrl(frontUrl);
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
