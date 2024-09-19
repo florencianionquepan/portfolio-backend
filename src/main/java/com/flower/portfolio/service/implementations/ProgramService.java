a7bda62a14724f4ff1ec27687a8fa3d704e2a1dd
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-19 20:25:59 -0300

    Create ProgramService.java

---
+++ src/main/java/com/flower/portfolio/service/implementations/ProgramService.java
@@ -0,0 +1,22 @@
+package com.flower.portfolio.service.implementations;
+
+import com.flower.portfolio.dto.ProgramDTO;
+import com.flower.portfolio.service.interfaces.IProgramService;
+
+public class ProgramService implements IProgramService {
+
+    @Override
+    public ProgramDTO getBy(String lastname) {
+        return null;
+    }
+
+    @Override
+    public ProgramDTO post(ProgramDTO dto) {
+        return null;
+    }
+
+    @Override
+    public ProgramDTO update(ProgramDTO dto) {
+        return null;
+    }
+}
