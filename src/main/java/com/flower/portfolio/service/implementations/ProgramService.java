61d3c3e5094885459d13824e9b545128f57be029
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-20 20:30:15 -0300

    Update ProgramService.java

--- src/main/java/com/flower/portfolio/service/implementations/ProgramService.java
+++ src/main/java/com/flower/portfolio/service/implementations/ProgramService.java
@@ -1,22 +1,35 @@
 package com.flower.portfolio.service.implementations;
 
 import com.flower.portfolio.dto.ProgramDTO;
+import com.flower.portfolio.dto.mapper.IProgramMapper;
+import com.flower.portfolio.model.AcademicProgram;
+import com.flower.portfolio.repository.ProgramRepository;
 import com.flower.portfolio.service.interfaces.IProgramService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ProgramService implements IProgramService {
 
+    private IProgramMapper mapper;
+    private ProgramRepository repo;
+    private PersonService personService;
+
     @Override
-    public ProgramDTO getBy(String lastname) {
-        return null;
+    public List<ProgramDTO> programsByPerson(Long idPerson) {
+        List<AcademicProgram> programs=this.repo.findByPersonId(idPerson);
+        return this.mapper.mapToListDTO(programs);
     }
 
     @Override
     public ProgramDTO post(ProgramDTO dto) {
-        return null;
+        AcademicProgram entity=this.mapper.mapToEntity(dto);
+        return this.mapper.mapToDTO(this.repo.save(entity));
     }
 
     @Override
-    public ProgramDTO update(ProgramDTO dto) {
+    public ProgramDTO update(ProgramDTO dto, Long id) {
         return null;
     }
 }
