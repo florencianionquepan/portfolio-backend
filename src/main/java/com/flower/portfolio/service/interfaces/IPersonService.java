314a23bdde22a193849441d7f4bd93ecfecc606d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-20 20:30:04 -0300

    Update IPersonService.java

--- src/main/java/com/flower/portfolio/service/interfaces/IPersonService.java
+++ src/main/java/com/flower/portfolio/service/interfaces/IPersonService.java
@@ -2,9 +2,11 @@ package com.flower.portfolio.service.interfaces;
 
 import com.flower.portfolio.dto.ContactInfoDTO;
 import com.flower.portfolio.dto.PersonDTO;
+import com.flower.portfolio.dto.PersonWithDetailsDTO;
 import com.flower.portfolio.model.Person;
 
 public interface IPersonService {
+    PersonWithDetailsDTO getAllData(String lastname);
     ContactInfoDTO getSensitiveData(String lastname);
     PersonDTO get(String lastname);
     PersonDTO post(PersonDTO person);
