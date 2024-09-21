22cb7372eae089c16002e06ac794b81d942c7aa3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-21 16:57:55 -0300

    Create ProgramController.java

---
+++ src/main/java/com/flower/portfolio/controller/ProgramController.java
@@ -0,0 +1,38 @@
+package com.flower.portfolio.controller;
+
+import com.flower.portfolio.dto.PersonDTO;
+import com.flower.portfolio.dto.ProgramDTO;
+import com.flower.portfolio.service.interfaces.IProgramService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/programs")
+public class ProgramController {
+    private final IProgramService service;
+    public Map<String,Object> bodyMessage= new HashMap<>();
+
+    public ProgramController(IProgramService service) {
+        this.service = service;
+    }
+
+    private ResponseEntity<?> successResponse(Object data){
+        bodyMessage.put("success",Boolean.TRUE);
+        bodyMessage.put("data",data);
+        return ResponseEntity.ok(bodyMessage);
+    }
+
+    @PostMapping("/person/{id}")
+    public ResponseEntity<?> createProfile(@PathVariable Long id, @RequestBody @Valid ProgramDTO dto){
+        ProgramDTO created=this.service.post(dto, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.successResponse(created));
+    }
+
+
+}
