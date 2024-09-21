0be4e9f28a47b6088c42537448a492ff4241a6f0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-21 16:22:06 -0300

    Create ExceptionDTO.java

---
+++ src/main/java/com/flower/portfolio/dto/response/ExceptionDTO.java
@@ -0,0 +1,14 @@
+package com.flower.portfolio.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ExceptionDTO {
+    private int estado;
+    private String mensaje;
+    private Map<String,String> detalle;
+}
