eb58b83b267f9fbb48bd5029c6921c0eb31d75be
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-20 21:06:08 -0300

    Update ProgramService.java

--- src/main/java/com/flower/portfolio/service/implementations/ProgramService.java
+++ src/main/java/com/flower/portfolio/service/implementations/ProgramService.java
@@ -3,18 +3,29 @@ package com.flower.portfolio.service.implementations;
 import com.flower.portfolio.dto.ProgramDTO;
 import com.flower.portfolio.dto.mapper.IProgramMapper;
 import com.flower.portfolio.model.AcademicProgram;
+import com.flower.portfolio.model.Person;
+import com.flower.portfolio.repository.PersonRepository;
 import com.flower.portfolio.repository.ProgramRepository;
 import com.flower.portfolio.service.interfaces.IProgramService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProgramService implements IProgramService {
 
-    private IProgramMapper mapper;
-    private ProgramRepository repo;
-    private PersonService personService;
+    private final IProgramMapper mapper;
+    private final ProgramRepository repo;
+    private final PersonRepository personRepo;
+
+    public ProgramService(IProgramMapper mapper,
+                          ProgramRepository repo,
+                          PersonRepository personRepo) {
+        this.mapper = mapper;
+        this.repo = repo;
+        this.personRepo = personRepo;
+    }
 
     @Override
     public List<ProgramDTO> programsByPerson(Long idPerson) {
@@ -23,13 +34,18 @@ public class ProgramService implements IProgramService {
     }
 
     @Override
-    public ProgramDTO post(ProgramDTO dto) {
+    public ProgramDTO post(ProgramDTO dto, Long idPerson) {
+        Optional<Person> oPerson=this.personRepo.findById(idPerson);
+        if(oPerson.isEmpty()){
+            //...lanzar excepcion
+        }
         AcademicProgram entity=this.mapper.mapToEntity(dto);
+        entity.setPerson(oPerson.get());
         return this.mapper.mapToDTO(this.repo.save(entity));
     }
 
     @Override
-    public ProgramDTO update(ProgramDTO dto, Long id) {
+    public ProgramDTO update(ProgramDTO dto, Long id, Long idPerson) {
         return null;
     }
 }
