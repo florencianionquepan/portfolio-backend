86c0dfb72f2bc2db07f6e0e1ba976bbe15691642
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-03 20:15:05 -0300

    Update PersonController.java

--- src/main/java/com/flower/portfolio/controller/PersonController.java
+++ src/main/java/com/flower/portfolio/controller/PersonController.java
@@ -2,8 +2,8 @@ package com.flower.portfolio.controller;
 
 import com.flower.portfolio.dto.ContactInfoDTO;
 import com.flower.portfolio.dto.PersonDTO;
-import com.flower.portfolio.dto.mapper.IPersonMapper;
 import com.flower.portfolio.service.interfaces.IPersonService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,13 +17,10 @@ import java.util.Map;
 public class PersonController {
 
     private final IPersonService personService;
-    private final IPersonMapper mapper;
     public Map<String,Object> mensajeBody= new HashMap<>();
 
-    public PersonController(IPersonService personService,
-                            IPersonMapper mapper) {
+    public PersonController(IPersonService personService) {
         this.personService = personService;
-        this.mapper = mapper;
     }
 
     private ResponseEntity<?> successResponse(Object data){
@@ -33,7 +30,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public ResponseEntity<?> createProfile(@Validated PersonDTO dto){
+    public ResponseEntity<?> createProfile(@Valid PersonDTO dto){
         PersonDTO created=this.personService.post(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(this.successResponse(created));
     }
