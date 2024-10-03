d676e5fcc679f7992ec97146f8543f373a89b2fa
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-03 20:52:30 -0300

    Create UserDTO.java

---
+++ src/main/java/com/flower/portfolio/auth/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.flower.portfolio.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String email;
+    private String name;
+    private String imageURL;
+    private String role;
+}
