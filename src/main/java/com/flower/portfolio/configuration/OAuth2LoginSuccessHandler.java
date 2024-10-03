41e05507666f5fe94f0e0c201bdca46bdaadf0b1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-03 20:52:25 -0300

    return UserDTO on success login

--- src/main/java/com/flower/portfolio/configuration/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/flower/portfolio/configuration/OAuth2LoginSuccessHandler.java
@@ -5,10 +5,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.userinfo.DefaultReactiveOAuth2UserService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
 
 @Component
 public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
@@ -21,5 +27,13 @@ public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSu
         this.setAlwaysUseDefaultTargetUrl(true);
         this.setDefaultTargetUrl(frontUrl);
         super.onAuthenticationSuccess(request, response, authentication);
+        UserDTO dto=this.getUserDetails(authentication);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userDtoJson = objectMapper.writeValueAsString(dto);
+        response.getWriter().write(userDtoJson);
+    }
     }
 }
