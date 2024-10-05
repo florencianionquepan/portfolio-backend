b7ee45676b326ecfc9a7b78c045997911b76a3b0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-05 19:22:15 -0300

    Update UserDTO.java

--- src/main/java/com/flower/portfolio/auth/dto/UserDTO.java
+++ src/main/java/com/flower/portfolio/auth/dto/UserDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class UserDTO {
     private String email;
+    private String username;
     private String name;
     private String imageURL;
     private String role;
