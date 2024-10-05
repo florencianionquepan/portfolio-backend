e101c31209cb50a7b65dc9de84ebb85a66b2b1d6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-05 19:22:19 -0300

    Create UserRepository.java

---
+++ src/main/java/com/flower/portfolio/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.flower.portfolio.repository;
+
+import com.flower.portfolio.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsernameAndProvider(String username, String provider);
+    Optional<User> findByEmailAndProvider(String email,String provider);
+}
