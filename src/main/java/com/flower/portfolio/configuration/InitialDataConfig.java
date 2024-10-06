66d43b46ba8869199ac866988d19692a66669a88
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-06 17:27:49 -0300

    Create InitialDataConfig.java

---
+++ src/main/java/com/flower/portfolio/configuration/InitialDataConfig.java
@@ -0,0 +1,29 @@
+package com.flower.portfolio.configuration;
+
+import com.flower.portfolio.model.Role;
+import com.flower.portfolio.model.RoleName;
+import com.flower.portfolio.repository.RoleRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InitialDataConfig {
+
+    private final RoleRepository roleRepository;
+
+    public InitialDataConfig(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @PostConstruct
+    public void init(){
+        Role role_user=new Role();
+        role_user.setId(1);
+        role_user.setName(RoleName.ROLE_USER);
+        this.roleRepository.save(role_user);
+        Role role_admin=new Role();
+        role_admin.setId(2);
+        role_admin.setName(RoleName.ROLE_ADMIN);
+        this.roleRepository.save(role_admin);
+    }
+}
