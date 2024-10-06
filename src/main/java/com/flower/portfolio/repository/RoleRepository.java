865cab41f6f17e9f9ea45a28dcb9abd352a270a3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-06 17:27:56 -0300

    Create RoleRepository.java

---
+++ src/main/java/com/flower/portfolio/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.flower.portfolio.repository;
+
+import com.flower.portfolio.model.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role,Long> {
+}
