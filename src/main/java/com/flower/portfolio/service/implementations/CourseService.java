e52e5756be0c4365509c6c84c2a5e5a8cfb49bf9
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 18:16:40 -0300

    Create CourseService.java

---
+++ src/main/java/com/flower/portfolio/service/implementations/CourseService.java
@@ -0,0 +1,57 @@
+package com.flower.portfolio.service.implementations;
+
+import com.flower.portfolio.dto.CourseDTO;
+import com.flower.portfolio.dto.mapper.ICourseMapper;
+import com.flower.portfolio.model.Course;
+import com.flower.portfolio.model.Person;
+import com.flower.portfolio.repository.ICourseRepository;
+import com.flower.portfolio.repository.IPersonRepository;
+import com.flower.portfolio.service.interfaces.ICourseService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService implements ICourseService {
+
+    private final ICourseRepository repo;
+    private final ICourseMapper mapper;
+    private final IPersonRepository personRepo;
+
+    public CourseService(ICourseRepository repo,
+                         ICourseMapper mapper,
+                         IPersonRepository personRepo) {
+        this.repo = repo;
+        this.mapper = mapper;
+        this.personRepo = personRepo;
+    }
+
+    @Override
+    public List<CourseDTO> getCoursesByPerson(Long idPerson) {
+        List<Course> courses=this.repo.findByPersonId(idPerson);
+        return this.mapper.mapToListDTO(courses);
+    }
+
+    @Override
+    public CourseDTO post(CourseDTO dto, Long idPerson) {
+        Optional<Person> oPerson=this.personRepo.findById(idPerson);
+        if(oPerson.isEmpty()){
+            //...lanzar excepcion
+        }
+        Course course=this.mapper.mapToEntity(dto);
+        course.setPerson(oPerson.get());
+        return this.mapper.mapToDTO(this.repo.save(course));
+    }
+
+    @Override
+    public CourseDTO put(CourseDTO dto, Long idC) {
+        Course courseModified = this.mapper.mapToEntity(dto);
+        Optional<Course> oCourse = this.repo.findById(idC);
+        if(oCourse.isEmpty()){
+            //...
+        }
+        courseModified.setId(idC);
+        return this.mapper.mapToDTO(this.repo.save(courseModified));
+    }
+}
