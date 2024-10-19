882c854e847f2f1a04bc4c7142918a75d9d8d510
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 15:41:11 -0300

    Update IUserMapper.java

--- src/main/java/com/flower/portfolio/auth/dto/mapper/IUserMapper.java
+++ src/main/java/com/flower/portfolio/auth/dto/mapper/IUserMapper.java
@@ -2,8 +2,11 @@ package com.flower.portfolio.auth.dto.mapper;
 
 import com.flower.portfolio.auth.dto.UserDTO;
 import com.flower.portfolio.model.User;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
 
 public interface IUserMapper {
     User mapToEntity(UserDTO dto, String provider);
+    UserDTO mapDefaultOAuth2UserToDto(DefaultOAuth2User auth2User, String provider);
+    UserDTO mapEntityToDTO(User user);
 }
