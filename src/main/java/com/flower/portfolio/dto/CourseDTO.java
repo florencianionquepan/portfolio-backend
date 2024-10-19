98a3f2a3ed95f41c8e3fb6b2d1318aa2f6a76aad
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 19:05:31 -0300

    Update CourseDTO.java

--- src/main/java/com/flower/portfolio/dto/CourseDTO.java
+++ src/main/java/com/flower/portfolio/dto/CourseDTO.java
@@ -1,7 +1,6 @@
 package com.flower.portfolio.dto;
 
 import com.flower.portfolio.dto.validator.PastDateRange;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -16,11 +15,11 @@ public class CourseDTO {
     @NotEmpty
     @Pattern(regexp = "^[a-zA-Z\\sÀ-ÿ]+$", message = "Title must contain only letters.")
     private String title;
-    @Pattern(regexp = "^[a-zA-Z\\sÀ-ÿ]+$", message = "Instructor must contain only letters.")
+    @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s()]+$", message = "Instructor must contain only letters, spaces and parentheses")
     private String instructor;
     @Pattern(regexp = "^[a-zA-Z\\sÀ-ÿ0-9]+$", message = "Duration must contain only letters and numbers")
     private String duration;
-    @Pattern(regexp = "^[a-zA-Z\\sÀ-ÿ]+$", message = "Description must contain only letters.")
+    @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s()\\-:.,]+$", message = "Description must contain only letters, spaces, parentheses, hyphens, colons, commas, and periods.")
     private String description;
     @PastDateRange
     private LocalDate startDate;
