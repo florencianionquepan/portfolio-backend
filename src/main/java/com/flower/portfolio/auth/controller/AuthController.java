2d6f21d40820c9b8d3e8ee7cdbcd8b1f80f573b9
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 15:44:17 -0300

    Update AuthController.java

--- src/main/java/com/flower/portfolio/auth/controller/AuthController.java
+++ src/main/java/com/flower/portfolio/auth/controller/AuthController.java
@@ -1,7 +1,8 @@
 package com.flower.portfolio.auth.controller;
 
 import com.flower.portfolio.auth.dto.UserDTO;
-import com.flower.portfolio.service.interfaces.IUserService;
+import com.flower.portfolio.auth.dto.mapper.IUserMapper;
+import com.flower.portfolio.auth.service.IUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/auth")
@@ -20,9 +21,12 @@ public class AuthController {
 
     public Map<String,Object> mensajeBody= new HashMap<>();
     public final IUserService service;
+    public final IUserMapper userMapper;
 
-    public AuthController(IUserService service) {
+    public AuthController(IUserService service,
+                          IUserMapper userMapper) {
         this.service = service;
+        this.userMapper = userMapper;
     }
 
     private ResponseEntity<?> successResponse(Object data){
@@ -33,27 +37,12 @@ public class AuthController {
 
     @GetMapping
     private ResponseEntity<?> getUserDetails(Authentication authentication){
-        UserDTO dto=new UserDTO();
         OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
-        if("github".equals(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId())){
-            DefaultOAuth2User principal= (DefaultOAuth2User) authentication.getPrincipal();
-            Map<String,Object> attributes=principal.getAttributes();
-            String name = getAttributeValue(attributes, "name");
-            String email = getAttributeValue(attributes, "email");
-            String avatarUrl = getAttributeValue(attributes, "avatar_url");
-            String username = getAttributeValue(attributes,"login");
-            dto.setEmail(email);
-            dto.setName(name);
-            dto.setImageURL(avatarUrl);
-            dto.setUsername(username);
-            this.service.createOrUpdateUser(dto,"github");
-        }
-        return this.successResponse(dto);
+        String provider=oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+        DefaultOAuth2User principal= (DefaultOAuth2User) authentication.getPrincipal();
+        UserDTO dto=this.userMapper.mapDefaultOAuth2UserToDto(principal,provider);
+        UserDTO created=this.service.createOrUpdateUser(dto,provider);
+        return this.successResponse(created);
     }
 
-    private String getAttributeValue(Map<String, Object> attributes, String key) {
-        return Optional.ofNullable(attributes.get(key))
-                .map(Object::toString)
-                .orElse("");
-    }
 }
