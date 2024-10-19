5264a8a41c3aec296228c3a30cd452a27fe2a7eb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 18:16:36 -0300

    Update ICourseService.java

--- src/main/java/com/flower/portfolio/service/interfaces/ICourseService.java
+++ src/main/java/com/flower/portfolio/service/interfaces/ICourseService.java
@@ -1,12 +1,11 @@
 package com.flower.portfolio.service.interfaces;
 
 import com.flower.portfolio.dto.CourseDTO;
-import com.flower.portfolio.model.Course;
 
 import java.util.List;
 
 public interface ICourseService {
-    List<Course> getCoursesByPerson(Long idPerson);
+    List<CourseDTO> getCoursesByPerson(Long idPerson);
     CourseDTO post(CourseDTO dto, Long idPerson);
     CourseDTO put(CourseDTO dto, Long idC);
 }
