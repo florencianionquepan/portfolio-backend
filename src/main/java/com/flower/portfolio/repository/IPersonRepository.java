80a7b2a0fd8ad0616053c2b55aaec4cc8c859517
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 20:46:12 -0300

    Update IPersonRepository.java

--- src/main/java/com/flower/portfolio/repository/IPersonRepository.java
+++ src/main/java/com/flower/portfolio/repository/IPersonRepository.java
@@ -1,9 +1,16 @@
 package com.flower.portfolio.repository;
 
 import com.flower.portfolio.model.Person;
+import com.flower.portfolio.model.WebProject;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface IPersonRepository extends CrudRepository<Person,Long> {
 
     Person findByLastName(String lastname);
+
+    @Query("SELECT wp FROM WebProject wp WHERE wp.person.id = :id")
+    List<WebProject> findAllByPersonId(Long id);
 }
