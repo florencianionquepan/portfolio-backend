1bf773ef1667e7277c05f5d7335b0aba90d33541
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 15:41:15 -0300

    Update UserMapper.java

--- src/main/java/com/flower/portfolio/auth/dto/mapper/UserMapper.java
+++ src/main/java/com/flower/portfolio/auth/dto/mapper/UserMapper.java
@@ -1,10 +1,17 @@
 package com.flower.portfolio.auth.dto.mapper;
 
 import com.flower.portfolio.auth.dto.UserDTO;
+import com.flower.portfolio.model.Role;
 import com.flower.portfolio.model.User;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class UserMapper implements IUserMapper{
@@ -35,5 +42,45 @@ public class UserMapper implements IUserMapper{
         user.setProvider(provider);
         user.setLastLogin(lastLogin);
         return user;
+    }
+
+    @Override
+    public UserDTO mapDefaultOAuth2UserToDto(DefaultOAuth2User auth2User, String provider) {
+        UserDTO dto=new UserDTO();
+        Map<String,Object> attributes=auth2User.getAttributes();
+        String name = getAttributeValue(attributes, "name");
+        String email = getAttributeValue(attributes, "email");
+        dto.setEmail(email);
+        dto.setName(name);
+        if(provider.equals("github")){
+            String avatarUrl = getAttributeValue(attributes, "avatar_url");
+            String username = getAttributeValue(attributes,"login");
+            dto.setUsername(username);
+            dto.setImageURL(avatarUrl);
+        } else if (provider.equals("google")) {
+            String avatarUrl = getAttributeValue(attributes, "picture");
+            dto.setImageURL(avatarUrl);
+        }
+        return dto;
+    }
+
+    private String getAttributeValue(Map<String, Object> attributes, String key) {
+        return Optional.ofNullable(attributes.get(key))
+                .map(Object::toString)
+                .orElse("");
+    }
+
+    @Override
+    public UserDTO mapEntityToDTO(User user) {
+        UserDTO dto=new UserDTO();
+        List<String> roles = user.getRoles().stream()
+                .map(u->u.getName().toString())
+                .collect(Collectors.toList());
+        dto.setUsername(user.getUsername());
+        dto.setEmail(user.getEmail());
+        dto.setName(user.getName());
+        dto.setImageURL(user.getImageURL());
+        dto.setRole(roles);
+        return dto;
     }
 }
