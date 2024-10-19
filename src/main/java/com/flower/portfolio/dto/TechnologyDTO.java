76974bd20c109e48b52c0c1af8d5ce0115591854
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 19:26:24 -0300

    Create TechnologyDTO.java

---
+++ src/main/java/com/flower/portfolio/dto/TechnologyDTO.java
@@ -0,0 +1,18 @@
+package com.flower.portfolio.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class TechnologyDTO {
+    private Long id;
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z.]+$", message = "Name must contain only letters and periods.")
+    private String name;
+    private String logoUrl;
+    @Pattern(regexp = "^[0-9.]+$", message = "Version must contain only numbers and periods.")
+    private String version;
+}
