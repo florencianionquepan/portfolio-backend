e1f58fd69d05573535b6c19ef1cf73c5f2b755e7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-19 21:23:02 -0300

    Update ITechnologyRepository.java

--- src/main/java/com/flower/portfolio/repository/ITechnologyRepository.java
+++ src/main/java/com/flower/portfolio/repository/ITechnologyRepository.java
@@ -3,5 +3,11 @@ package com.flower.portfolio.repository;
 import com.flower.portfolio.model.Technology;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ITechnologyRepository extends CrudRepository<Technology,Long> {
+    List<Technology> findDistinctByProjectsId(Long id);
+    Optional<Technology> findByNameAndVersion(String name, String version);
+
 }
