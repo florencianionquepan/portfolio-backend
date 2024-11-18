a677d1da8f6aacd16c050a258789e4ffa0afbddb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-18 18:25:48 -0300

    Update ImageUploadedMapper.java

--- src/main/java/com/flower/portfolio/external/mapper/ImageUploadedMapper.java
+++ src/main/java/com/flower/portfolio/external/mapper/ImageUploadedMapper.java
@@ -1,6 +1,7 @@
 package com.flower.portfolio.external.mapper;
 
 import com.flower.portfolio.model.Image;
+import com.flower.portfolio.model.WebProject;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,17 +11,18 @@ import java.util.stream.Collectors;
 @Component
 public class ImageUploadedMapper {
 
-    public Image mapToImage(Map<String, Object> result){
+    public Image mapToImage(Map<String, Object> result, WebProject project){
         Image image=new Image();
         image.setUrl((String)(result.get("url")));
         image.setImageId((String) result.get("public_id"));
         image.setName((String) result.get("original_filename"));
+        image.setProject(project);
         return image;
     }
 
-    public List<Image> mapToListImage(List<Map<String, Object>> results){
+    public List<Image> mapToListImage(List<Map<String, Object>> results, WebProject project){
         return results.stream()
-                .map(this::mapToImage)
+                .map(result -> mapToImage(result, project))
                 .collect(Collectors.toList());
     }
 }
