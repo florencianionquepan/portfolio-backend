800facd6c09f9dfaf9e0ffb5d35256efc3ba5bf4
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-18 18:26:10 -0300

    Update IWebProjectService.java

--- src/main/java/com/flower/portfolio/service/interfaces/IWebProjectService.java
+++ src/main/java/com/flower/portfolio/service/interfaces/IWebProjectService.java
@@ -1,11 +1,12 @@
 package com.flower.portfolio.service.interfaces;
 
 import com.flower.portfolio.dto.WebProjectDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface IWebProjectService {
     List<WebProjectDTO> projectsByPerson(Long id);
-    WebProjectDTO create(WebProjectDTO dto, Long idPerson);
+    WebProjectDTO create(WebProjectDTO dto, MultipartFile[] files, Long idPerson);
     WebProjectDTO update(WebProjectDTO dto, Long id);
 }
