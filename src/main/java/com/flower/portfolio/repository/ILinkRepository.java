9cd09ac95e4e43c79ca124895b9c7c8bccb03724
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-18 14:03:35 -0300

    Create ILinkRepository.java

---
+++ src/main/java/com/flower/portfolio/repository/ILinkRepository.java
@@ -0,0 +1,7 @@
+package com.flower.portfolio.repository;
+
+import com.flower.portfolio.model.Link;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ILinkRepository extends CrudRepository<Link,Long> {
+}
