3f3fdc48fbcbf5b848ea7e4791a76cebd97e0ed4
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-18 14:04:47 -0300

    Update TechnologyController.java

--- src/main/java/com/flower/portfolio/controller/TechnologyController.java
+++ src/main/java/com/flower/portfolio/controller/TechnologyController.java
@@ -1,42 +1,41 @@
 package com.flower.portfolio.controller;
 
 import com.flower.portfolio.dto.TechnologyDTO;
+import com.flower.portfolio.model.Technology;
 import com.flower.portfolio.service.interfaces.ITechnologyService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/technologies")
 public class TechnologyController {
 
     private final ITechnologyService service;
-    public Map<String,Object> bodyMessage= new HashMap<>();
 
     public TechnologyController(ITechnologyService service) {
         this.service = service;
     }
 
-    private ResponseEntity<?> successResponse(Object data){
-        bodyMessage.put("success",Boolean.TRUE);
-        bodyMessage.put("data",data);
-        return ResponseEntity.ok(bodyMessage);
+    @GetMapping()
+    public ResponseEntity<?> getAll(){
+        List<TechnologyDTO> technologies=this.service.getAll();
+        return ResponseEntity.ok(technologies);
     }
 
     @PostMapping
-    public ResponseEntity<?> createProgram(@RequestBody @Valid TechnologyDTO dto){
+    public ResponseEntity<?> createTechnology(@RequestBody @Valid TechnologyDTO dto){
         TechnologyDTO created=this.service.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
     @PutMapping("/{idT}")
-    public ResponseEntity<?> updateProgram(@PathVariable Long idT,
+    public ResponseEntity<?> updateTechnology(@PathVariable Long idT,
                                            @RequestBody @Valid TechnologyDTO dto){
         TechnologyDTO modified=this.service.update(dto,idT);
-        return this.successResponse(modified);
+        return ResponseEntity.ok(modified);
     }
 }
