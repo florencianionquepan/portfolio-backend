c8294f91218f56c6fe3c8527710d15936907fcc9
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-18 19:25:02 -0300

    Update IProjectMapper.java

--- src/main/java/com/flower/portfolio/dto/mapper/IProjectMapper.java
+++ src/main/java/com/flower/portfolio/dto/mapper/IProjectMapper.java
@@ -2,11 +2,9 @@ package com.flower.portfolio.dto.mapper;
 
 import com.flower.portfolio.dto.WebProjectDTO;
 import com.flower.portfolio.model.WebProject;
-import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
 public interface IProjectMapper {
     WebProjectDTO mapToDTO(WebProject entity);
     WebProject mapToEntity(WebProjectDTO dto);
