cc369f3af2e647c9ec39147162cede275a13e973
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-18 14:04:32 -0300

    Update WebProjectDTO.java

--- src/main/java/com/flower/portfolio/dto/WebProjectDTO.java
+++ src/main/java/com/flower/portfolio/dto/WebProjectDTO.java
@@ -1,11 +1,16 @@
 package com.flower.portfolio.dto;
 
 import com.flower.portfolio.dto.validator.ValueOfEnum;
+import com.flower.portfolio.model.Image;
+import com.flower.portfolio.model.Link;
 import com.flower.portfolio.model.Status;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class WebProjectDTO {
@@ -15,8 +20,8 @@ public class WebProjectDTO {
     @Pattern(regexp = "^[a-zA-Z\\sÀ-ÿ]+$", message = "Title must contain only letters.")
     private String title;
     private String description;
-    private String url;
-    @ValueOfEnum(enumClass=Status.class)
+    private LocalDate endDate;
     private Status status;
-    private String imageURL;
+    private List<Image> images;
+    private List<Link> links;
 }
