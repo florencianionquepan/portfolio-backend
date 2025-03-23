84b1470335d508709301c20a69c9971e4e1ea064
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-23 14:34:41 -0300

    create endpoint to edit project

--- src/main/java/com/flower/portfolio/controller/ProjectController.java
+++ src/main/java/com/flower/portfolio/controller/ProjectController.java
@@ -23,10 +23,10 @@ public class ProjectController {
         this.service = service;
     }
 
-    private ResponseEntity<?> validarFiles(MultipartFile[] multipartFiles){
-        if (multipartFiles == null || multipartFiles.length == 0) {
+    private ResponseEntity<?> validarFiles(MultipartFile[] multipartFiles, Boolean projectNew){
+        if ((multipartFiles == null || multipartFiles.length == 0) && projectNew) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No images have been uploaded.");
-        }else{
+        }else if((multipartFiles!=null)){
             for (MultipartFile multipartFile : multipartFiles){
                 BufferedImage bi= null;
                 try {
@@ -46,11 +46,23 @@ public class ProjectController {
     public ResponseEntity<?> createProject(@RequestPart("project") @Valid WebProjectDTO dto,
                                            @PathVariable Long idPerson,
                                            @RequestParam(required = false) MultipartFile[] files){
-        ResponseEntity<?> response=this.validarFiles(files);
+        ResponseEntity<?> response=this.validarFiles(files, true);
         if(response.getStatusCode()!=HttpStatus.OK){
             return response;
         }
         WebProjectDTO dtoCreated=this.service.create(dto, files, idPerson);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dtoCreated);
+    }
+
+    @PutMapping(path = "/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE,MediaType.APPLICATION_OCTET_STREAM_VALUE})
+    public ResponseEntity<?> editProject(@RequestPart("project") @Valid WebProjectDTO dto,
+                                         @PathVariable Long id,
+                                         @RequestParam(required = false) MultipartFile[] files){
+        ResponseEntity<?> response=this.validarFiles(files, false);
+        if(response.getStatusCode()!=HttpStatus.OK){
+            return response;
+        }
+        WebProjectDTO dtoCreated=this.service.update(dto, id, files);
         return ResponseEntity.status(HttpStatus.CREATED).body(dtoCreated);
     }
 
