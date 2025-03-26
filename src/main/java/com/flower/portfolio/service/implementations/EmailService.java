6478b9feb57d568aaf2ca578f0c1527cbeb4a2ab
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-25 21:00:33 -0300

    Create EmailService.java

---
+++ src/main/java/com/flower/portfolio/service/implementations/EmailService.java
@@ -0,0 +1,37 @@
+package com.flower.portfolio.service.implementations;
+
+import com.flower.portfolio.dto.ContactFormDTO;
+import com.flower.portfolio.service.interfaces.IEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService implements IEmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value(("${spring.mail.username}"))
+    private String emailDestinatary;
+
+    @Override
+    public Boolean sendMessage(ContactFormDTO contactForm, String ip) {
+        try{
+            SimpleMailMessage email = new SimpleMailMessage();
+            email.setTo(emailDestinatary);
+            email.setSubject(contactForm.getSubject());
+            email.setText("Mensaje de: " + contactForm.getEmail() + "\n\n" + contactForm.getDescription() + "\n\nIP del remitente: " + ip);
+            email.setFrom(contactForm.getEmail());
+
+            mailSender.send(email);
+            return true;
+        } catch (MailException e) {
+            System.err.println("Error al enviar el correo: " + e.getMessage());
+            return false;
+        }
+    }
+}
