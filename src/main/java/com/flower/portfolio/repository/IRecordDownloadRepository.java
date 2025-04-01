bcb807926156c63cca27ee35271bca3b9f36481d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-04-01 20:27:41 -0300

    Create IRecordDownloadRepository.java

---
+++ src/main/java/com/flower/portfolio/repository/IRecordDownloadRepository.java
@@ -0,0 +1,8 @@
+package com.flower.portfolio.repository;
+
+
+import com.flower.portfolio.model.RecordDownload;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IRecordDownloadRepository extends CrudRepository<RecordDownload,Long> {
+}
