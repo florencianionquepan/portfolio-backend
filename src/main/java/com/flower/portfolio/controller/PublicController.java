546d656bd4629622fc81aeb2eafee73919ffd31e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-04-01 20:09:31 -0300

    create endpoint to download resume

--- src/main/java/com/flower/portfolio/controller/PublicController.java
+++ src/main/java/com/flower/portfolio/controller/PublicController.java
@@ -5,9 +5,17 @@ import com.flower.portfolio.dto.PersonWithDetailsDTO;
 import com.flower.portfolio.service.interfaces.IEmailService;
 import com.flower.portfolio.service.interfaces.IPersonService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/public")
@@ -15,6 +23,9 @@ public class PublicController {
 
     private final IPersonService personService;
     private final IEmailService emailService;
+
+    @Value("${resume.path}")
+    private String RESUME_PATH;
 
     public PublicController(IPersonService personService,
                             IEmailService emailService) {
@@ -30,10 +41,39 @@ public class PublicController {
 
     @PostMapping("/contact")
     public ResponseEntity<?> sendContactForm(@RequestBody ContactFormDTO contact, HttpServletRequest httpRequest){
-        String clientIp = httpRequest.getRemoteAddr();
+        String clientIp = this.getClientIp(httpRequest);
         Boolean sent=this.emailService.sendMessage(contact, clientIp);
         return sent
                 ? ResponseEntity.ok("Message sent successfully")
                 : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send message");
+    }
+
+    @GetMapping("/resume")
+    public ResponseEntity<?> getResume(HttpServletRequest request){
+        String clientIp = this.getClientIp(request);
+        try {
+            Path path = Paths.get(RESUME_PATH);
+            Resource resource = new UrlResource(path.toUri());
+            if (!resource.exists() || !resource.isReadable()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=resume.pdf")
+                    .body(resource);
+        } catch (Exception e) {
+            //logger.error("Error al descargar el CV: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr(); // Si no hay X-Forwarded-For, usar la IP directa
+        } else {
+            ip = ip.split(",")[0].trim(); // Tomar la primera IP en caso de múltiples proxies
+        }
+        return ip;
     }
 }
